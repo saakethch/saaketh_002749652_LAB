c87c9b08a9ff9d78d94f0259b46c2be3185d41f8
Saaketh Ch <chaganty.s@northeastern.edu>
2023-01-22 15:11:24 -0500

    Lab1.done

---
+++ src/main/java/Model/Address.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 15512
+ */
+public class Address {
+    String streetName;
+    String aptNumber;
+    String zipCode;
+    String city;
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getAptNumber() {
+        return aptNumber;
+    }
+
+    public void setAptNumber(String aptNumber) {
+        this.aptNumber = aptNumber;
+    }
+    
+    
+}
---
+++ src/main/java/Model/Person.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 15512
+ */
+public class Person {
+    String firstName;
+    Address workAddress;
+    Address homeAddress;
+    String lastName;
+    String collegeName;
+    String NUID;
+    String phone;
+    String email;
+
+    public Person(){
+        this.firstName= "";
+        this.lastName = "";
+        this.workAddress = new Address();
+        this.homeAddress = new Address();
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(Address workAddress) {
+        this.workAddress = workAddress;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getNUID() {
+        return NUID;
+    }
+
+    public void setNUID(String NUID) {
+        this.NUID = NUID;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public static void main(String args[]){
+        Person obj = new Person("Saaketh","Ch");
+        obj.getAddress().setStreetName("Huntington");
+    }
+    
+}
